71028a1e76b8db69eb9230c688bd6b3659310ce0
Jakub Groncki <jakub.groncki@gmail.com>
2018-04-30 09:34:20 +0200

    Added basic assets

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,12 @@
+require('../images/crossword.png');
 require('../scss/app.scss');
+require('font-awesome/css/font-awesome.css');
+
+const $ = require('jquery');
+
+$(document).ready(() => {
+	$('a').on('click', function(e) {
+		e.preventDefault();
+		history.pushState({}, '', $(this).attr('href'));
+	});
+});
